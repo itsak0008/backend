c024fee7941e8c910013c89a3236da3e9d219cbd
Alok kumar <itsak0008@gmail.com>
2025-04-14 17:16:02 +0530

    new update

---
+++ script.js
@@ -0,0 +1,20 @@
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//     console.log("hello", i);
+// }
+
+//
+// let info = require("./fruits");
+// console.log("fruits :", info);
+
+// console.log(ans.sum(4, 5));
+// console.log(ans.sub(4, 3));
+// console.log(ans.mul(4, 5));
+// console.log(ans.div(4, 2));
+
+import { sum, PI } from "./math.js";
+import { generate } from "random-words";
+
+console.log(sum(1, 2), PI);
+console.log(generate());
