e904f920a29a7ca5450621feda003aa4313fa815
Alok kumar <itsak0008@gmail.com>
2025-04-14 17:00:32 +0530

    new update

---
+++ math.js
@@ -0,0 +1,20 @@
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
+export const sub = (a, b) => a - b;
+export const div = (a, b) => a / b;
+
+// const g = 9.8;
+export const PI = 3.14;
+
+// let obj = {
+//     sum: sum,
+//     sub: sub,
+//     mul: mul,
+//     div: div,
+//     g: g,
+//     pI: PI,
+// };
+
+// module.exports = obj;
+
+
